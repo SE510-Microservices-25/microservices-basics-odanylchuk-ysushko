e89f0403df9ed74fc332a6a2965661b72ad756ba
yurii_sushko <ysushko@kse.org.ua>
2025-03-31 18:51:39 +0300

    kubernetes

--- Tasks/Programm.cs
+++ Tasks/Programm.cs
@@ -23,13 +23,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
