262ae7a633386e99a84a50d21009cb7f7623d6fe
odanylchuk <odanylchuk@kse.org.ua>
2025-04-11 14:34:50 +0300

    added proper testing of endpoints

--- src/test/java/com/example/demo/UserControllerTest.java
+++ src/test/java/com/example/demo/UserControllerTest.java
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,8 +28,10 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(UserController.class)
-@WithMockUser(roles = "USER")
+@Import(TestSecurityConfig.class)
+@WithMockUser(username = "testuser", roles = {"USER"})
 public class UserControllerTest {
 
     @Autowired
@@ -36,13 +40,13 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
     
-    @Mock
+    @MockBean
     private UserCommandService userCommandService;
 
-    @Mock
+    @MockBean
     private UserQueryService userQueryService;
 
-    @Mock
+    @MockBean
     private OutboxPublisherService outboxPublisherService;
 
     private User testUser;
