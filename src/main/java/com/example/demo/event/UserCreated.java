5cb7de3db9c381c5800511793392ee1f75cc819f
odanylchuk <odanylchuk@kse.org.ua>
2025-04-11 14:30:26 +0300

    added jackson config

--- src/main/java/com/example/demo/event/UserCreated.java
+++ src/main/java/com/example/demo/event/UserCreated.java
@@ -2,10 +2,21 @@ package com.example.demo.event;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserCreated extends UserEvent implements Serializable {
 
     public UserCreated(long userId, String username, String token) {
         super(userId, username, token);
+    }
+
+    @JsonCreator
+    public UserCreated(
+           @JsonProperty("id") Long id,
+           @JsonProperty("username") String username,
+           @JsonProperty("token") String token) {
+        super(id, username, token);
     }
     
 }
